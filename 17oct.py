18ae5feef25a806c7f3b508441949fa5cd1f7319
akash <aaku17.10.1998@gmail.com>
2018-11-21 14:51:13 +0530

    17oct complete

--- 17oct.py
+++ 17oct.py
@@ -1,19 +1,19 @@
-#1
+#1.
 '''a= [1,2,3,4,5,6,7]
 print(a[::-1])
 '''
 
-#2
+#2.
 '''a= ("hello")
 print(a.upper())
 '''
 
-#3
+#3.
 '''a= input("enter anything: ")
 print(a.split())
 '''
 
-#4
+#4.
 '''a= input("enter a palindrome: ")
 b= a[::-1]
 if(a==b):
